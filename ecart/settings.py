e403f6154854d86499e47cfbe32a75f9068833ef
Ranjith R <72377429+Ranjithr-007@users.noreply.github.com>
2021-05-01 12:16:02 +0530

    Update settings.py
    final commit

--- ecart/settings.py
+++ ecart/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'j=^3^-^5@(ne=rsu85)xv=fzqdfsk=cpbzz#n^iu8_!(xz&77r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
