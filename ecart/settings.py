ceeec0c57470444bb41b7093396814b796f4d6fc
Ranjith R <72377429+Ranjithr-007@users.noreply.github.com>
2021-07-22 13:11:41 +0530

    Update settings.py

--- ecart/settings.py
+++ ecart/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ecart',
-        'USER': 'ranjith',
+        'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost'
     }
